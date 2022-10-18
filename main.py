164914cf445f9e87820e0d37d5714e4c0b86a976
Linh Nguyen <linhnm>
2022-10-18 23:32:11 +0700

    doing task 2

---
+++ main.py
@@ -0,0 +1,16 @@
+# import isort
+
+
+# def get_stdlib_packages():
+#     module_names = isort.stdlibs.py38.stdlib
+#     external_packages = set()
+#     for name in module_names:
+#         if name[0] == '_' or name == 'this' or name == 'antigravity':
+#             continue
+#         external_packages.add(name)
+#     return external_packages
+
+
+# if __name__ == '__main__':
+#     externel_packages = get_stdlib_packages()
+#     print(externel_packages)
