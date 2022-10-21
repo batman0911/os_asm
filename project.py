55388ed5b23d8d32e29b670813b5d686324827e3
Linh Nguyen <linhnguyen.code@gmail.com>
2022-10-21 22:46:55 +0700

    task 5

---
+++ project.py
@@ -0,0 +1,21 @@
+"""
+Template for the COMP1730/6730 project assignment, S2 2022.
+The assignment specification is available on the course web
+site, at https://cs.anu.edu.au/courses/comp1730/assessment/project/
+
+Collaborators: <list the UIDs of ALL members of your project group here>
+"""
+
+def analyse_stdlib():
+    pass
+
+
+# The section below will be executed when you run this file.
+# Use it to run tests of your analysis function on the data
+# files provided.
+
+if __name__ == '__main__':
+    NAME = 'John Smith'
+    ID   = 'u1234567'
+    print(f'My name is {NAME}, my id is {ID}, and these are my findings for Project COMP1730.2022.S2')
+    analyse_stdlib()
